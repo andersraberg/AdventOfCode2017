9c579f49a9e84e9eecca2ae9f1db5844bb508ff6
Anders Råberg <anders.raberg@gmail.com>
2017-12-07 21:32:50 +0100

    First version if Day1 puzzle

---
+++ day1/src/test/java/se/anders_raberg/adventofcode/test/Day1Test.java
@@ -0,0 +1,11 @@
+package se.anders_raberg.adventofcode.test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Day1Test {
+	@Test 
+	public void testAppHasAGreeting() {
+		assertTrue(true);
+	}
+}
