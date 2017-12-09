976ce49d8637061add4cd05b53a37daf238081e8
Anders Råberg <anders.raberg@gmail.com>
2017-12-09 14:55:10 +0100

    First version if Day3 puzzle

---
+++ day3/src/main/java/se/anders_raberg/adventofcode/Day3.java
@@ -0,0 +1,50 @@
+package se.anders_raberg.adventofcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Day3 {
+	private static final Logger LOGGER = Logger.getLogger(Day3.class.getName());
+
+	private static final Distance RIGHT_STEP = new Distance(1, 0);
+	private static final Distance UP_STEP = new Distance(0, 1);
+	private static final Distance LEFT_STEP = new Distance(-1, 0);
+	private static final Distance DOWN_STEP = new Distance(0, -1);
+	
+	private static final int TARGET_VALUE = 347991;
+
+	public static void main(String[] args) {
+		final List<Distance> steps = new ArrayList<>();
+
+		int i = 1;
+		while (true) {
+			if (i % 2 != 0) {
+				addSteps(steps, RIGHT_STEP, i);
+				addSteps(steps, UP_STEP, i);
+			} else {
+				addSteps(steps, LEFT_STEP, i);
+				addSteps(steps, DOWN_STEP, i);
+			}
+
+			if (steps.size() >= TARGET_VALUE) {
+				break;
+			}
+
+			i++;			
+		}
+
+		Distance distance = steps.stream()
+				.limit(TARGET_VALUE - 1L)
+				.reduce(new Distance(0, 0), Distance::add);
+		
+		LOGGER.info("Horizontal: " + distance.getHorizontal() + ", Vertical: " + distance.getVertical());
+		LOGGER.info("Manhattan: " + (Math.abs(distance.getHorizontal()) + Math.abs(distance.getVertical())));
+	}
+
+	private static void addSteps(List<Distance> list, Distance step, int number) {
+		for (int i = 0; i < number; i++) {
+			list.add(step);
+		}
+	}
+}
