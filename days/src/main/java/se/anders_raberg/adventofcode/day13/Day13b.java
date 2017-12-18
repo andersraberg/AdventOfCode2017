49cce122a72f86b3cfba508810397087f5553026
Anders Råberg <anders.raberg@gmail.com>
2017-12-18 18:52:50 +0100

    Day13 puzzle. (Thanks to Peter's modulo-based algorithm)

---
+++ days/src/main/java/se/anders_raberg/adventofcode/day13/Day13b.java
@@ -0,0 +1,55 @@
+package se.anders_raberg.adventofcode.day13;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day13b {
+	private static final Logger LOGGER = Logger.getLogger(Day13b.class.getName());
+
+	public static void run(String inputPath) throws IOException {
+		Map<Integer, Layer> layers = new HashMap<>();
+		try (Stream<String> input = Files.lines(Paths.get(inputPath + "/input13.txt"))) {
+			input.forEach(l -> {
+				List<Integer> parsedLine = Arrays.stream(l.split("\\W+")).map(Integer::parseInt)
+						.collect(Collectors.toList());
+
+				int layerpos = parsedLine.get(0);
+				int layerDepth = parsedLine.get(1);
+				layers.put(layerpos, new Layer(layerDepth));
+			});
+		}
+
+		int lastLayer = layers.keySet().stream().max(Integer::compare).get();
+		int d = 0;
+		while(true) {
+			layers.values().forEach(Layer::resetScanner);			
+			int delay = d;
+			layers.values().forEach(l -> l.moveScanner(delay));
+
+			boolean caught = false; 
+			for (int i = 0; i < lastLayer + 1; i++) {
+				if (layers.containsKey(i) && layers.get(i).scannerInFirstPos()) {
+					caught = true;
+					break;
+				}
+
+				layers.values().forEach(Layer::moveScanner);
+			}
+
+			if (!caught) {
+				break;
+			}
+			d++;
+		}
+
+		LOGGER.info("Delay: " + d);
+	}
+}
