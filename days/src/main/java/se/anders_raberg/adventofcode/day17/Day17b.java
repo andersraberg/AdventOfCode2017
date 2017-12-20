927d5a496193d674ebb41a745354e32bd122cb18
Anders Råberg <anders.raberg@gmail.com>
2017-12-20 22:27:00 +0100

    Indentation fix

--- days/src/main/java/se/anders_raberg/adventofcode/day17/Day17b.java
+++ days/src/main/java/se/anders_raberg/adventofcode/day17/Day17b.java
@@ -3,20 +3,20 @@ package se.anders_raberg.adventofcode.day17;
 import java.util.logging.Logger;
 
 public class Day17b {
-	private static final Logger LOGGER = Logger.getLogger(Day17b.class.getName());
-	private static final int INPUT = 369;
+    private static final Logger LOGGER = Logger.getLogger(Day17b.class.getName());
+    private static final int INPUT = 369;
 
-	public static void run() {
-	    int valueAtIndexOne = 0;
-	    int index = 0;
-	    for (int i = 1; i <= 50_000_000; i++) {
+    public static void run() {
+        int valueAtIndexOne = 0;
+        int index = 0;
+        for (int i = 1; i <= 50_000_000; i++) {
             index = (index + INPUT) % i + 1;
-            
+
             if (index == 1) {
-                valueAtIndexOne = i;   
+                valueAtIndexOne = i;
             }
-	    }
-	    
-	    LOGGER.info("Value at index one: " + valueAtIndexOne);
-	}
+        }
+
+        LOGGER.info("Value at index one: " + valueAtIndexOne);
+    }
 }
