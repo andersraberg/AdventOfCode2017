fd43ff860623f68695509b4a3bc2e4a1d66282e3
Anders Råberg <anders.raberg@gmail.com>
2017-12-27 21:54:40 +0100

    Day 21 work in progress cont'd 2

--- days/src/main/java/se/anders_raberg/adventofcode/day21/Day21a.java
+++ days/src/main/java/se/anders_raberg/adventofcode/day21/Day21a.java
@@ -3,7 +3,6 @@ package se.anders_raberg.adventofcode.day21;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
