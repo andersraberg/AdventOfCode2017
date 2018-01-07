46a7de06461a03a6879c47af6766f63e253e57a5
Anders Råberg <anders.raberg@gmail.com>
2018-01-07 17:02:30 +0100

    More unit tests

--- days/src/test/java/se/anders_raberg/adventofcode/day21/test/ArtPatternTest.java
+++ days/src/test/java/se/anders_raberg/adventofcode/day21/test/ArtPatternTest.java
@@ -1,5 +1,6 @@
 package se.anders_raberg.adventofcode.day21.test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -109,22 +110,196 @@ public class ArtPatternTest {
         assertEquals(new ArtPattern(expected), flipped);
     }
 
-    @Ignore
     @Test
-    public void testGetHorzontallyFlipped() {
-        fail("Not yet implemented");
+    public void testGetVerticallyFlipped3x3() {
+        String[][] startPattern = {
+                {"A", "B", "C"},
+                {"D", "E", "F"},
+                {"G", "H", "I"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+
+        String[][] expected = {
+                {"G", "H", "I"},
+                {"D", "E", "F"},
+                {"A", "B", "C"}};
+
+        ArtPattern flipped = testee.getVerticallyFlipped();
+        assertEquals(new ArtPattern(expected), flipped);
     }
 
-    @Ignore
     @Test
-    public void testSplit() {
-        fail("Not yet implemented");
+    public void testGetHorzontallyFlipped2x2() {
+        String[][] startPattern = {
+                {"X", "Y"},
+                {"Z", "W"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+
+        String[][] expected = {
+                {"Y", "X"},
+                {"W", "Z"}};
+
+        ArtPattern flipped = testee.getHorzontallyFlipped();
+        assertEquals(new ArtPattern(expected), flipped);
     }
 
-    @Ignore
+    @Test
+    public void testGetHorzontallyFlipped3x3() {
+        String[][] startPattern = {
+                {"A", "B", "C"},
+                {"D", "E", "F"},
+                {"G", "H", "I"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+
+        String[][] expected = {
+                {"C", "B", "A"},
+                {"F", "E", "D"},
+                {"I", "H", "G"}};
+
+        ArtPattern flipped = testee.getHorzontallyFlipped();
+        assertEquals(new ArtPattern(expected), flipped);
+    }
+
+    @Test
+    public void testSplit2x2() {
+        String[][] startPattern = {
+                {"X", "Y"},
+                {"Z", "W"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+        ArtPattern[][] expectedSplit = {{new ArtPattern(startPattern)}};
+        ArtPattern[][] splitPattern = testee.split();
+        assertArrayEquals(expectedSplit, splitPattern);
+    }
+
+    @Test
+    public void testSplit3x3() {
+        String[][] startPattern = {
+                {"A", "B", "C"},
+                {"D", "E", "F"},
+                {"G", "H", "I"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+        ArtPattern[][] expectedSplit = {{new ArtPattern(startPattern)}};
+        ArtPattern[][] splitPattern = testee.split();
+        assertArrayEquals(expectedSplit, splitPattern);
+    }
+
+    @Test
+    public void testSplitAndJoin4x4() {
+        String[][] startPattern = {
+                {"0", "1", "2", "3"},
+                {"4", "5", "6", "7"},
+                {"8", "9", "A", "B"},
+                {"C", "D", "E", "F"}};
+        
+        
+        String[][] expect00 = {
+                {"0", "1"},
+                {"4", "5"}};
+        
+        String[][] expect01 = {
+                {"2", "3"},
+                {"6", "7"}};
+
+        String[][] expect10 = {
+                {"8", "9"},
+                {"C", "D"}};
+
+        String[][] expect11 = {
+                {"A", "B"},
+                {"E", "F"}};
+
+        ArtPattern testee = new ArtPattern(startPattern);
+        ArtPattern[][] expectedSplit = {
+                {new ArtPattern(expect00), new ArtPattern(expect01)},
+                {new ArtPattern(expect10), new ArtPattern(expect11)}};
+        ArtPattern[][] splitPattern = testee.split();
+        assertArrayEquals(expectedSplit, splitPattern);
+        
+        assertEquals(testee, ArtPattern.joinPatterns(expectedSplit));
+        
+    }
+
+    @Test
+    public void testSplitAndJoin6x6() {
+        String[][] startPattern = {
+                {"0", "1", "2", "3", "4", "5"},
+                {"6", "7", "8", "9", "A", "B"},
+                {"C", "D", "E", "F", "G", "H"},
+                {"I", "J", "K", "L", "M", "N"},
+                {"O", "P", "Q", "R", "S", "T"},
+                {"U", "V", "W", "X", "Y", "Z"}};
+        
+        
+        String[][] expect00 = {
+                {"0", "1", "2"},
+                {"6", "7", "8"},
+                {"C", "D", "E"}};
+        
+        String[][] expect01 = {
+                {"3", "4", "5"},
+                {"9", "A", "B"},
+                {"F", "G", "H"}};
+        
+        String[][] expect10 = {
+                {"I", "J", "K"},
+                {"O", "P", "Q"},
+                {"U", "V", "W"}};
+        
+        String[][] expect11 = {
+                {"L", "M", "N"},
+                {"R", "S", "T"},
+                {"X", "Y", "Z"}};
+        
+
+        ArtPattern testee = new ArtPattern(startPattern);
+        ArtPattern[][] expectedSplit = {
+                {new ArtPattern(expect00), new ArtPattern(expect01)},
+                {new ArtPattern(expect10), new ArtPattern(expect11)}};
+        ArtPattern[][] splitPattern = testee.split();
+        assertArrayEquals(expectedSplit, splitPattern);
+        assertEquals(testee, ArtPattern.joinPatterns(expectedSplit));
+    }
+
     @Test
     public void testSubPattern() {
-        fail("Not yet implemented");
+        String[][] startPattern = {
+                {"0", "1", "2", "3"},
+                {"4", "5", "6", "7"},
+                {"8", "9", "A", "B"},
+                {"C", "D", "E", "F"}};
+        
+        ArtPattern testee = new ArtPattern(startPattern);
+        String[][] expected1 = {
+                {"0"}};
+
+        ArtPattern subPattern1 = testee.subPattern(0, 0, 1, 1);
+        assertEquals(new ArtPattern(expected1), subPattern1);
+
+        String[][] expected2 = {
+                {"F"}};
+
+        ArtPattern subPattern2 = testee.subPattern(3, 3, 1, 1);
+        assertEquals(new ArtPattern(expected2), subPattern2);
+
+        String[][] expected3 = {
+                {"5", "6"},
+                {"9", "A"}};
+
+        ArtPattern subPattern3 = testee.subPattern(1, 1, 2, 2);
+        assertEquals(new ArtPattern(expected3), subPattern3);
+
+        String[][] expected4 = {
+                {"1", "2", "3"},
+                {"5", "6", "7"},
+                {"9", "A", "B"}};
+
+        ArtPattern subPattern4 = testee.subPattern(0, 1, 3, 3);
+        assertEquals(new ArtPattern(expected4), subPattern4);
+
     }
 
     @Ignore
